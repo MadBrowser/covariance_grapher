b9dd9b583e1d3beee07fd60956e3d5abdff9cdcb
stgomorales <shagos@MacBook-Pro-de-santiago.local>
2014-07-29 16:23:34 -0400

    add user interface

--- progcov2.py
+++ progcov2.py
@@ -1,67 +1,123 @@
-#!/usr/bin/python
-#coding=utf-8
+from Tkinter import *
+from tkFileDialog import *
+import tkMessageBox
+from sys import *
+from time import *
+import os
 from obspy.core import read
 import numpy as np
 import Gnuplot
-import wx, os
 
-raw_input("ABRIR ARCHIVO")#stop para presentar
+class App:
+	
+	def __init__(self,master):
+		
+		frame = Frame(master)
+		frame.pack()
+		self.plotLabel = Label(frame, text = "GraphCov")
+		self.plotLabel.grid(rowspan = 1,columnspan = 3,sticky = W+E+N+S)
+		self.plotLabel.grid(row = 1, column = 1)
 
-app = wx.PySimpleApp()
-wildcard = 	"Archivos SAC (*.SAC)|*.SAC|" \
-			"Archivos Python (*.py)|*.py|" \
-        	"Archivos de texto (*.txt)|*.txt|" \
-        	"All files (*.*)|*.*"
-dialog = wx.FileDialog(None, "Prog_Cov", os.getcwd(), "", wildcard, wx.OPEN)
-if dialog.ShowModal() == wx.ID_OK: 
-    print dialog.GetFilename() 
-    lol = str(dialog.GetPath())
+		self.fileNameText = Entry(frame, bd = 2,state = DISABLED)
+		self.fileNameText.grid(columnspan = 1,sticky = W+E+N+S)
+		self.fileNameText.grid(row=2, column = 1)
 
-covarianza = []
-tiempo = []
+		self.buttonFile = Button(frame, text= "Abrir", command = self.openFile)
+		self.buttonFile.grid(columnspan = 1,sticky = W+E+N+S)
+		self.buttonFile.grid(row = 2, column = 2)
 
-st = read(lol)
-tr = st[0]
-print tr.stats
-sr = int(tr.stats.sampling_rate) #muestra
-w = int(raw_input('Ingrese Intervalo: ')) #cantidad de mediciones
+		self.label = Label(frame, text = "Intervalo")
+		self.label.grid(rowspan = 1,columnspan = 3,sticky = W+E+N+S)
+		self.label.grid(row = 3,column = 1)
 
-cant_medi = len(tr)
-cant_vect = len(tr) / (sr * w) #cantidad de vectores
+		self.intervalText = Entry(frame, bd = 2)
+		self.intervalText.grid(columnspan = 2)
+		self.intervalText.grid(row=4, column = 1)
 
-i = 1
+		self.plotButton = Button(frame, text = "Plot!", command = self.ploting)
+		self.plotButton.grid(rowspan = 1,columnspan = 3,sticky=W+E+N+S)
+		self.plotButton.grid(row=5,column=1)
 
-for i in range(1, cant_vect + 1):
-	rang1 = (i - 1) * w * sr
-	rang2 = (i * w *sr) - 1
-	seg = i * w
-	vect = tr.data[rang1:rang2]
-	cov = float(np.cov(vect))
-	covarianza.append([cov,seg])
+	def openFile(self):
+		global fileSelectString
+		'''
+		self.fileOpt = options = {}
+		options['defaultextension'] = '*.SAC'
+		options['filetypes'] = [('Todos Los Archivos', '.*'),('Archivos SAC','.SAC'),('Archivos Python', '.py'),('Archivos de Texto', '*.txt')]
+		options['initialdir'] = '\\home'
+		options['initialfile'] = 'temblor.SAC'
+		options['parent'] = root
+		options['title'] = 'Abrir Archivo'
+		FILEOPENOPTIONS = dict(defaultextension='.SAC',filetypes=[('Todos los Archivos','*.*'), ('Archivo SAC','*.SAC')])
+		#filename = askopenfilename(**FILEOPENOPTIONS)
+		filename = askopenfilename(**self.fileOpt)'''
+		fileSelect = askopenfilename()
+		fileSelectString = str(fileSelect)
+		fileName = os.path.split(fileSelect)[1]
+		self.fileNameText.config(state = NORMAL)
+		self.fileNameText.insert(0,fileName)
 
-for j in range(0,cant_vect,w):
-    tiempo.append(float(j))
+	def ploting(self):
+		global intervalString
+		intervalString = self.intervalText.get()
+		if(fileSelectString == "" or intervalString == ""):
+			tkMessageBox.showinfo("Problemas","Debe seleccionar un archivo e ingresar un intervalo")
 
-
-
-raw_input("MATRIZ COVARIANZA")
-print "MATRIZ DE COVARIANZA"
-print "**********************************"
-print covarianza
-print np.matrix(covarianza)
-print "**********************************"
-raw_input("GRAFICAR COVARIANZA")
-g = Gnuplot.Gnuplot()
-d1=Gnuplot.Data(covarianza, using='2:1',with_="lines", title="Covarianza")
-
-g.title('GRAFICA DE LA COVARIANZA')
-g.xlabel('Intervalo de Tiempo')
-g.ylabel('Valor Covarianza')
-g.plot(d1)
-
-raw_input("GRAFICAR LA SEÑAL")
-tr.plot()
+		else:
+			interval = int(intervalString)
+			self.covariance(fileSelectString,interval)
 
 
-raw_input("El programa a terminado. Presione una tecla para terminar.....")
+
+	def covariance(self,fname, inter):
+		global covarianceArray
+		global timeArray
+		print fname
+		print inter
+		st = read(fname)
+		tr = st[0]
+		print tr.stats
+		print tr.stats.sampling_rate
+		sr = int(tr.stats.sampling_rate)
+		samples = len(tr)
+		arrays = len(tr)/ (sr * inter)
+
+		i = 1
+
+		for i in range(1, arrays + 1):
+			rang1 = (i - 1) * inter * sr
+			rang2 = (i * inter * sr) - 1
+			seg = i * inter
+			vector = tr.data[rang1:rang2]
+			cov = float(np.cov(vector))
+			covarianceArray.append([cov,seg])
+
+		for j in range(0,arrays,inter):
+			timeArray.append(float(j))
+
+		'''	
+		print "Matriz covarianza"
+		print "*****************"
+		print covarianceArray
+		print np.matrix(covarianceArray) '''
+
+		g = Gnuplot.Gnuplot()
+		data = Gnuplot.Data(covarianceArray, using = '2:1', with_ = "lines", title ="Covarianza")
+		g.title('Grafica de Covarianza')
+		g.xlabel('Intervalo de Tiempo')
+		g.ylabel('Valor Covarianza')
+		g.plot(data)
+		tr.plot()
+		#tkMessageBox.showinfo(":)","Vale, Vale!")
+
+
+
+fileSelectString = ""
+intervalString = ""
+covarianceArray = []
+timeArray = []
+root = Tk()
+root.title("Test")
+app=App(root)
+root.mainloop()
 
